e3ea71e8de42cf110d5a525183883d179d709bd4
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-08 14:21:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ class Driver{
     return tYear - this.startDate.getFullYear() - 1;
   }
 }
+
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+}
