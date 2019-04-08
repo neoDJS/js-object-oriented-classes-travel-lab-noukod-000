4fff434ee24485a8f62c8eb261f2ea7009d82d3f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-08 14:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,25 @@ class Route{
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
+
+  blocksTravelled(){
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+    let horizontalDistance = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+
+    if(this.beginningLocation.horizontal === this.endingLocation.horizontal){
+        return verticalDistance;
+    }else if (this.beginningLocation.vertical === this.endingLocation.vertical){
+        return horizontalDistance;
+    }else{
+        return verticalDistance + horizontalDistance;
+    }
+  }
+
+  estimatedTime(peak){
+    if(peak){
+        return this.blocksTravelled()/2
+    } else {
+        return this.blocksTravelled()/3
+    }
+  }
 }
